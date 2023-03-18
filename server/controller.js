11c21432c49f3cadd8deebaa225afe0eca6d1d99
Dwayne Foreman <dwayneforemanjr@yahoo.com>
2023-03-18 15:10:59 -0400

    first commot

---
+++ server/controller.js
@@ -0,0 +1,121 @@
+require("dotenv").config();
+const { CONNECTION_STRING } = process.env;
+const { request, response } = require("express");
+const Sequelize = require("Sequelize");
+
+const sequelize = new Sequelize(CONNECTION_STRING, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+let nextEmp = 5;
+
+module.exports = {
+  getUpcomingAppointments: (req, res) => {
+    sequelize
+      .query(
+        `select a.appt_id, a.date, a.service_type, a.notes, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = false
+        order by a.date desc;`
+      )
+      .then((dbRes) => res.status(200).send(dbRes[0]))
+      .catch((err) => console.log(err));
+  },
+
+  approveAppointment: (req, res) => {
+    let { apptId } = req.body;
+
+    sequelize
+      .query(
+        `UPDATE cc_appointments
+        SET approved = true
+        WHERE apptId = ${apptId}
+        
+        insert into cc_emp_appts (emp_id, appt_id)
+        values (${nextEmp}, ${apptId}),
+        (${nextEmp + 1}, ${apptId});
+        `
+      )
+      .then((dbRes) => {
+        res.status(200).send(dbRes[0]);
+        nextEmp += 2;
+      })
+      .catch((err) => console.log(err));
+  },
+
+  getAllClients: (request, response) => {
+    sequelize
+      .query(
+        `SELECT * FROM cc_users AS u
+            JOIN cc_clients AS c
+            ON u.user_id = c.user_id`
+      )
+      .then((dbResult) => response.status(200).send(dbResult[0]))
+      .catch((error) => console.log(error));
+  },
+
+  getPendingAppointments: (request, response) => {
+    sequelize
+      .query(
+        `SELECT * FROM cc_appointments
+            WHERE approved = false
+            ORDER BY date DESC `
+      )
+
+      .then((dbResult) => response.status(200).send(dbResult[0]))
+      .catch((error) => console.log(error));
+  },
+
+  getPastAppointments: (request, response) => {
+    sequelize
+      .query(
+        `select a.appt_id, a.date, a.service_type, a.notes, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;`
+      )
+
+      .then((dbResult) => response.status(200).send(dbResult[0]))
+      .catch((error) => console.log(error));
+  },
+
+  getPastAppointments: (request, response) => {
+    sequelize
+      .query(
+        `select a.appt_id, a.date, a.service_type, a.notes, a.approved, a.completed, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;`
+      )
+
+      .then((dbResult) => response.status(200).send(dbResult[0]))
+      .catch((error) => console.log(error));
+  },
+
+  completeAppointment: (request, response) => {
+    let { apptId } = request.body;
+    sequelize
+      .query(
+        `UPDATE cc_appointments a
+        SET approved = true
+        WHERE apptId = ${apptId};`
+      )
+
+      .then((dbResult) => response.status(200).send(dbResult[0]))
+      .catch((error) => console.log(error));
+  },
+};
